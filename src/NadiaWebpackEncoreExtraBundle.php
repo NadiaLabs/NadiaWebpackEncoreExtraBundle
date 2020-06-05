d27df4ba96e399939241441f72ff57c234ebc679
Leo <leo.on.the.earth@gmail.com>
2020-06-05 17:56:21 +0800

    Add missing bundle class

---
+++ src/NadiaWebpackEncoreExtraBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the NadiaWebpackEncoreExtraBundle package.
+ *
+ * (c) Leo <leo.on.the.earth@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nadia\Bundle\NadiaWebpackEncoreExtraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class NadiaWebpackEncoreExtraBundle
+ */
+class NadiaWebpackEncoreExtraBundle extends Bundle
+{
+}
